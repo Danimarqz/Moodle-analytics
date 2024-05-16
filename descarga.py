032310dc51fccf579cac52b91575ae2f524e7198
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2024-05-16 16:09:03 +0200

    fix - normalizar código centro

--- descarga.py
+++ descarga.py
@@ -5,21 +5,28 @@ import glob
 import json
 import pandas as pd
 from dotenv import load_dotenv
-from merge import merge
 
-# URLs
+# Cargar variables de entorno
 load_dotenv()
+
+# Leer el archivo JSON con la información de los centros
+with open('codigo_centros.json') as f:
+    nombreCentros = json.load(f)
+
+# Normalizar los nombres de los centros (eliminar ceros a la izquierda)
+nombreCentros_normalized = {k.lstrip('0'): v for k, v in nombreCentros.items()}
+
+# Leer el archivo JSON con las URLs de los informes
 with open('onlysavia.json') as f:
     dict = json.load(f)
-# config
+
+# Configuración inicial
 dfs = {}
 username = os.getenv('CAMPUS_USERNAME')
 password = os.getenv('CAMPUS_PASSWORD')
-
 archivo_destino = 'Savia Centros.xlsx'
+output_directory = os.getcwd()
 
-json_centros = 'nombre_centros.json'
-output_directory = "centros"
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
 
@@ -27,6 +34,7 @@ login_payload = {
     'username': username,
     'password': password,
 }
+
 def descarga_informe(login, file_link, file_name):
     session = requests.Session()
     login_response = session.post(login, data=login_payload)
@@ -37,48 +45,59 @@ def descarga_informe(login, file_link, file_name):
         print('Login successful. Initiating file download...' + file_name)
         try:
             file_response = session.get(file_link)
-        except:
-            print(file_response.status_code)
-
-        with open(file_name, 'wb') as file:
-            file.write(file_response.content)
-        print('File downloaded successfully.')
-        session.close()
+            with open(file_name, 'wb') as file:
+                file.write(file_response.content)
+            print('File downloaded successfully.')
+        except Exception as e:
+            print(f"Error downloading file {file_name}: {e}")
+        finally:
+            session.close()
 
 for key in dict:
     descarga_informe(dict[key]['login'], dict[key]['file'], dict[key]['output'])
 
-#csv a xlsx
-for csvfile in glob.glob(os.path.join('.','*.csv')):
+# Procesar los archivos CSV y convertirlos en un Excel con hojas separadas por centro
+writer = pd.ExcelWriter(os.path.join(output_directory, archivo_destino), engine='xlsxwriter')
+
+for csvfile in glob.glob(os.path.join('.', '*.csv')):
     try:
         df = pd.read_csv(csvfile, encoding='utf8')
         if 'Fecha fin' in df.columns:
             df['Fecha fin'] = pd.to_datetime(df['Fecha fin'], format='%d/%m/%Y', errors='coerce')
             today = datetime.now().date()
             valid_date_mask = ~df['Fecha fin'].isna() & (df['Fecha fin'].dt.date >= today)
-            filtered_df = df[valid_date_mask]
-            filtered_df.loc[:,'Fecha fin'] = filtered_df['Fecha fin'].dt.strftime('%d/%m/%Y')
-            filtered_df.loc[:,'Nota Examen final'] = filtered_df['Nota Examen final'].str.replace('%', '').astype(float)
+            filtered_df = df[valid_date_mask].copy()  # Hacer una copia explícita del DataFrame filtrado
+            filtered_df.loc[:, 'Fecha fin'] = filtered_df['Fecha fin'].dt.strftime('%d/%m/%Y')
+            filtered_df.loc[:, 'Nota Examen final'] = filtered_df['Nota Examen final'].str.replace('%', '').astype(float)
             name = os.path.splitext(os.path.basename(csvfile))[0]
             grupos = filtered_df.groupby('Centro')
 
             for centro, grupo in grupos:
-                dfs[centro] = grupo
+                # Quita los 0
+                centro_sin0 = centro.lstrip('0')
+                # Añade los 0
+                codigo_centro = '000' + centro_sin0
+                grupo['Centro'] = codigo_centro
+                if codigo_centro in dfs:
+                    dfs[codigo_centro] = pd.concat([dfs[codigo_centro], grupo], ignore_index=True)
+                else:
+                    dfs[codigo_centro] = grupo
         else:
             name = os.path.splitext(os.path.basename(csvfile))[0]
             dfs[name] = df
     except Exception as e:
         print(f"Error processing {csvfile}: {e}")
 
-#delete csv
-dir = os.getcwd()
+# Guardar cada DataFrame en una hoja separada del archivo Excel
+for centro, df in dfs.items():
+    df.to_excel(writer, sheet_name=centro[:31], index=False)
 
-dir_files = os.listdir(dir)
+writer.close()  # Usar close() en lugar de save()
+
+# Eliminar archivos CSV
+dir_files = os.listdir(os.getcwd())
 
 for file in dir_files:
     if file.endswith(".csv"):
-        ruta_completa = os.path.join(dir, file)
-        os.remove(ruta_completa)
+        os.remove(file)
         print(f"Archivo {file} eliminado correctamente.")
-
-merge(dfs, os.path.join(output_directory, archivo_destino))
--- merge.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def merge(dfs, archivo_destino):
-    with pd.ExcelWriter(archivo_destino, engine='xlsxwriter') as writer:
-        for name, df in dfs.items():
-            df.to_excel(writer, sheet_name=name, index=False)
