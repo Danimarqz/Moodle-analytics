b651485e13560c0668053683019c50dd71b49739
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2024-05-31 08:42:27 +0200

    filter those who have fecha baja

--- descarga.py
+++ descarga.py
@@ -66,7 +66,7 @@ for csvfile in glob.glob(os.path.join('.', '*.csv')):
             df['Fecha fin'] = pd.to_datetime(df['Fecha fin'], format='%d/%m/%Y', errors='coerce')
             today = datetime.now().date()
             # valid_date_mask = ~df['Fecha fin'].isna() & (df['Fecha fin'].dt.date >= today)
-            filtered_df = df  # Hacer una copia explícita del DataFrame filtrado
+            filtered_df = df[df['F_BAJA'].isna()]  # filtra el df para que se muestren los valores nulos
             filtered_df.loc[:, 'Fecha fin'] = filtered_df['Fecha fin'].dt.strftime('%d/%m/%Y')
             filtered_df.loc[:, 'Nota Examen final'] = filtered_df['Nota Examen final'].str.replace('%', '').astype(float)
             name = os.path.splitext(os.path.basename(csvfile))[0]
